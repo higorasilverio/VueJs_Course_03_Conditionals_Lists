555a36d825641f78bfe2dff8f1066a3c2b6c93a2
higorasilverio <higorasilverio@outlook.com>
2020-07-21 19:02:20 -0300

    Conditional formatation and lists

---
+++ app.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#ch',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		book: {
+			title: 'Lord of the Rings',
+			author: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		student: {
+			id: 10,
+			name: 'Maria',
+			grades: [7.67, 8.33, 6.98, 9.21]
+		},
+		active: false,
+		cellphones: ['Samsung', 'Motorola', 'Asus', 'Xiaomi', 'Apple']
+	}
+})
